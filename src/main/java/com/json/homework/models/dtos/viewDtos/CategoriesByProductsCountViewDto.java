e052f65e5031fd4456281686f8cf7135baf60c5f
Elena <helena.liliya@gmail.com>
2020-03-21 23:58:45 +0200

    ---Initial Commit---

--- src/main/java/com/json/homework/models/dtos/viewDtos/CategoriesByProductsCountViewDto.java
+++ src/main/java/com/json/homework/models/dtos/viewDtos/CategoriesByProductsCountViewDto.java
@@ -1,13 +1,6 @@
 package com.json.homework.models.dtos.viewDtos;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-import jdk.jfr.Name;
-
-import javax.persistence.Transient;
-import java.util.Set;
 
 public class CategoriesByProductsCountViewDto {
     @Expose
